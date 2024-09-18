498a561d42eb90e433963e43c955716febc57c4b
DevServer24 <developmentserver160@gmail.com>
2024-09-18 06:17:49 -0700

    changes api

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,7 +1,11 @@
 import serverless from "serverless-http";
 import server from "../../src/index";
 import { Router } from "express";
+import type{ Request,Response } from "express";
 const router = Router()
 server.use('/api',router)
+server.get('/',(req:Request,res:Response) =>{
 
+    res.send('Hello World')
+})
 export const handler = serverless(server)
